74a07e9a88c46ccdc6d40ea81c50eac9b387c2e8
Rufat Lazhansky <rufat.lazhansky@dtigames.com>
2017-03-17 14:07:10 +0300

    SobelEdgeDetector

---
+++ SobelEdgeDetector/Assets/EdgeDetectorTest.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using DG.Tweening;
+
+
+public class EdgeDetectorTest : MonoBehaviour
+{
+    private const bool AdjustBorder = true;
+
+    //private readonly int[,] matrix =
+    //{
+    //    {1,1,1,1,0,0,0,0,0,0},
+    //    {1,1,1,1,0,0,0,0,0,0},
+    //    {1,1,1,1,0,0,1,1,0,0},
+    //    {0,0,0,0,0,0,1,1,1,0},
+    //    {0,0,1,1,1,1,1,1,1,0},
+    //    {0,0,1,1,1,1,1,1,1,0},
+    //    {0,0,1,1,1,1,1,1,1,0},
+    //};
+
+    private readonly int[,] matrix =
+    {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1},
+        {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1},
+        {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1},
+        {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0},
+    };
+
+    private Vector3 _brushPosition;
+    private float[,] _edgeMatrix;
+    private int[,] _enumeratedEdges;
+
+
+    void Start()
+    {
+        _edgeMatrix = SimpleEdgeDetector.Prewitt(matrix, AdjustBorder);
+        _enumeratedEdges = EnumerateEdges(_edgeMatrix);
+        MoveBrushPosition(_enumeratedEdges);
+    }
+
+    private void OnDrawGizmos()
+    {
+        int h = matrix.GetLength(0);
+        int w = matrix.GetLength(1);
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (matrix[i, j] == 1)
+                {
+                    Gizmos.color = Color.green;
+                }
+                else
+                {
+                    Gizmos.color = Color.grey;
+                }
+
+                Gizmos.DrawWireCube(new Vector3(j, -i, 0), Vector3.one);
+            }
+        }
+
+        if (_edgeMatrix != null)
+        {
+            Gizmos.color = Color.black;
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    //if (_edgeMatrix[i, j] > 0)
+                    //{
+                    //    if (!AdjustBorder)
+                    //    {
+                    //        Gizmos.color = Color.Lerp(Color.grey, Color.black, (_edgeMatrix[i, j] - 1) / 10f);
+                    //    }
+                    //    Gizmos.DrawCube(new Vector3(j, -i, 0), 0.9f * Vector3.one);
+                    //}
+
+                    if(_enumeratedEdges[i, j] > 0)
+                    {
+                        Gizmos.color = Color.Lerp(Color.white, Color.black, (_enumeratedEdges[i, j] - 1) / 1f);
+                        Gizmos.DrawCube(new Vector3(j, -i, 0), 0.9f * Vector3.one);
+                    }                    
+                }
+            }
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_brushPosition, 1f);
+    }
+
+    private void MoveBrushPosition(int[,] edgeMatrix)
+    {
+        List<Vector3> edgePoints = new List<Vector3>();
+        Stack<Vector3> points = new Stack<Vector3>();
+        int h = matrix.GetLength(0);
+        int w = matrix.GetLength(1);
+
+        float[,] converterd = new float[h, w];
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                converterd[i, j] = edgeMatrix[i, j];
+            }
+        }
+
+        MoveBrushPosition(converterd);
+    }
+
+    private void MoveBrushPosition(float[,] edgeMatrix)
+    {
+        List<Vector3> edgePoints = new List<Vector3>();
+        List<Vector3> shuffledPoints = new List<Vector3>();
+        Stack<Vector3> points = new Stack<Vector3>();
+        
+        int h = matrix.GetLength(0);
+        int w = matrix.GetLength(1);
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if(edgeMatrix[i,j] > 0)
+                {
+                    edgePoints.Add(new Vector3(j, -i, 0));
+                }
+            }
+        }
+
+        //for (int i = 0, c = edgePoints.Count; i < c; i++)
+        //{
+        //    int count = edgePoints.Count;
+        //    int randomIndex = UnityEngine.Random.Range(0, count);
+
+        //    shuffledPoints.Add(edgePoints[randomIndex]);
+        //    edgePoints.RemoveAt(randomIndex);
+        //}
+
+        //foreach (Vector3 point in shuffledPoints.OrderBy(value => edgeMatrix[-(int)value.y, (int)value.x]))
+        //{
+        //    points.Push(point);
+        //}
+
+        foreach (Vector3 point in edgePoints.OrderBy(value => edgeMatrix[-(int)value.y, (int)value.x]).ThenBy(value => value.x))
+        {
+            points.Push(point);
+        }
+
+        NextMove(points);
+    }
+
+    private void NextMove(Stack<Vector3> points)
+    {
+        if (points.Count > 0)
+        {
+            Vector3 nextPoint = points.Peek();
+
+            while (points.Count > 0 && _brushPosition.x == points.Peek().x)
+            {
+                nextPoint = points.Pop();
+            }
+
+            float distance = (nextPoint - _brushPosition).magnitude;
+            float speed = 20f;
+            float time = distance / speed;
+
+            DG.Tweening.Core.DOGetter<Vector3> getter = () => _brushPosition;
+            DG.Tweening.Core.DOSetter<Vector3> setter = (value) => _brushPosition = value;
+            DOTween.To(getter, setter, nextPoint, time).OnComplete(() => NextMove(points));
+        }
+    }
+
+    private int[,] EnumerateEdges(float[,] edgeMatrix)
+    {
+        int h = edgeMatrix.GetLength(0);
+        int w = edgeMatrix.GetLength(1);
+
+        int[,] result = new int[edgeMatrix.GetLength(0), edgeMatrix.GetLength(1)];
+        List<Vector2> temp = new List<Vector2>();
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (edgeMatrix[i, j] > 0)
+                {
+                    temp.Add(new Vector2(i, j));
+                }
+            }
+        }
+
+        int counter = 0;
+
+        while(temp.Count > 0)
+        {
+            bool canContinue = false;
+
+            for (int i = 0, c = temp.Count; i < c; i++)
+            {
+                Vector2 point = temp[i];
+                int x = (int)point.x;
+                int y = (int)point.y;
+
+                for (int k = -1; k <= 1; k++)
+                {
+                    for (int m = -1; m <= 1; m++)
+                    {
+                        int dx = x + k;
+                        int dy = y + m;
+
+                        if (dx >= 0 && dx < h && dy >= 0 && dy < w)
+                        {
+                            int val = result[dx, dy];
+
+                            if (val > 0)
+                            {
+                                temp.RemoveAt(i);
+                                result[x, y] = val;
+                                canContinue = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if(canContinue)
+                    {
+                        break;
+                    }
+                }
+                if (canContinue)
+                {
+                    break;
+                }
+            }
+
+            if(canContinue)
+            {
+                continue;
+            }
+
+            counter++;
+            Vector2 firstPoint = temp[0];
+            int fx = (int)firstPoint.x;
+            int fy = (int)firstPoint.y;
+            temp.RemoveAt(0);
+            result[fx, fy] = counter;
+        }
+
+        return result;
+    }
+}
---
+++ SobelEdgeDetector/Assets/SimpleEdgeDetector.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// For binary images only
+/// </summary>
+public class SimpleEdgeDetector
+{
+    private static readonly int[,] SobelHorizontalKernel =
+    {
+        { -1, 0, 1 },
+        { -2, 0, 2 },
+        { -1, 0, 1 }
+    };
+
+    private static readonly int[,] SobelVerticalKernel =
+    {
+        { -1, -2, -1 },
+        {  0,  0,  0 },
+        {  1,  2,  1 }
+    };
+
+    private static readonly int[,] PrewittHorizontalKernel =
+    {
+        { -1, 0, 1 },
+        { -1, 0, 1 },
+        { -1, 0, 1 }
+    };
+
+    private static readonly int[,] PrewittVerticalKernel =
+    {
+        { -1, -1, -1 },
+        {  0,  0,  0 },
+        {  1,  1,  1 }
+    };
+
+    private static readonly int[,] RobertsCrossHorizontalKernel =
+    {
+        { 1,  0 },
+        { 0, -1 }
+    };
+
+    private static readonly int[,] RobertsCrossVerticalKernel =
+    {
+        {  0, 1 },
+        { -1, 0 }
+    };
+
+
+    public static float[,] Sobel(int[,] matrix, bool adjustBorder = false)
+    {
+        return Convolve(SobelHorizontalKernel, SobelVerticalKernel, matrix, adjustBorder);
+    }
+
+    public static float[,] Prewitt(int[,] matrix, bool adjustBorder = false)
+    {
+        return Convolve(PrewittHorizontalKernel, PrewittVerticalKernel, matrix, adjustBorder);
+    }
+
+    public static float[,] RobertsCross(int[,] matrix, bool adjustBorder = false)
+    {
+        return Convolve(RobertsCrossHorizontalKernel, RobertsCrossVerticalKernel, matrix, adjustBorder);
+    }
+
+    private static float[,] Convolve(int[,] HorizontalKernel, int[,] VerticalKernel, int[,] matrix, bool adjustBorder)
+    {
+        int h = matrix.GetLength(0);
+        int w = matrix.GetLength(1);
+        int kh = HorizontalKernel.GetLength(0);
+        int kw = HorizontalKernel.GetLength(1);
+        int dkh = kh / 2;
+        int dkw = kw / 2;
+
+        float[,] result = new float[matrix.GetLength(0), matrix.GetLength(1)];
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                int horizontalConvolutionResult = 0;
+                int verticalConvolutionResult = 0;
+
+                for (int k = 0; k < kh; k++)
+                {
+                    for (int m = 0; m < kw; m++)
+                    {
+                        int verticalOffset = i + k - dkh;
+                        int hrizontalOffset = j + m - dkw;
+                        int pixel = 0;
+
+                        if (verticalOffset >= 0 && verticalOffset < h && hrizontalOffset >= 0 && hrizontalOffset < w)
+                        {
+                            pixel = matrix[verticalOffset, hrizontalOffset];
+                        }
+
+                        horizontalConvolutionResult += pixel * HorizontalKernel[k, m];
+                        verticalConvolutionResult += pixel * VerticalKernel[k, m];
+                    }
+                }
+
+                if (!adjustBorder)
+                {
+                    result[i, j] = Mathf.Sqrt(horizontalConvolutionResult * horizontalConvolutionResult +
+                        verticalConvolutionResult * verticalConvolutionResult);
+                }
+                else
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        result[i, j] = Mathf.Clamp01(horizontalConvolutionResult * horizontalConvolutionResult +
+                            verticalConvolutionResult * verticalConvolutionResult);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
